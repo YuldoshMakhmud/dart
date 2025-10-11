6a31968ae6177446d47e506eb36bb5c2d50b7b7d
unknown <yuldoshmakhmud1@gmail.com>
2025-10-11 19:29:01 +0900

    lesson

---
+++ readMe.dart
@@ -0,0 +1,30 @@
+// 01_intro.dart             // Dart tiliga kirish, nima uchun Dart
+// 02_setup_environment.dart  // Dart SDK va IDE o‘rnatish
+// 03_hello_world.dart        // Birinchi dastur: Hello World
+// 04_variables.dart          // O‘zgaruvchilar
+// 05_data_types.dart         // Ma’lumot turlari: int, double, String, bool
+// 06_constants_final.dart    // Const va final
+// 07_operators.dart          // Operatorlar: +, -, *, /, %, ++, --
+// 08_strings.dart            // String bilan ishlash
+// 09_numbers.dart            // Sonlar bilan ishlash
+// 10_lists.dart              // Ro‘yxatlar (List)
+// 11_sets.dart               // Setlar
+// 12_maps.dart               // Maplar
+// 13_conditionals.dart       // If, else, switch
+// 14_loops.dart              // For, while, do-while
+// 15_functions.dart          // Funksiyalar
+// 16_optional_parameters.dart// Optional va named parameters
+// 17_classes.dart            // Klasslar va obyektlar
+// 18_constructors.dart       // Konstruktorlar
+// 19_inheritance.dart        // Meros olish
+// 20_mixins.dart             // Mixins
+// 21_interfaces.dart         // Interfaces
+// 22_abstract_classes.dart   // Abstract klasslar
+// 23_exceptions.dart         // Xatoliklar bilan ishlash
+// 24_async_await.dart        // Async va await
+// 25_futures_streams.dart    // Future va Stream
+// 26_packages_imports.dart   // Paketlar va import
+// 27_generics.dart           // Generiklar
+// 28_collections_methods.dart// List, Set, Map metodlari
+// 29_file_io.dart            // Fayl bilan ishlash
+// 30_project_example.dart    // Kichik loyiha misoli
