3b5cee08c88dae955e6c43a0738c0359da75361b
unknown <yuldoshmakhmud1@gmail.com>
2025-11-04 23:51:41 +0900

    dart

--- masala1.dart
+++ masala1.dart
@@ -63,3 +63,29 @@ void main() {
     print('Eng kichigi: $engKichik');
   }
 }
+// import 'dart:io';
+
+// void main() {
+//   stdout.write('1-sonni kiriting: ');
+//   String? a = stdin.readLineSync();
+
+//   stdout.write('2-sonni kiriting: ');
+//   String? b = stdin.readLineSync();
+
+//   stdout.write('3-sonni kiriting: ');
+//   String? c = stdin.readLineSync();
+
+//   int? n1 = int.tryParse(a ?? '');
+//   int? n2 = int.tryParse(b ?? '');
+//   int? n3 = int.tryParse(c ?? '');
+
+//   if (n1 == null || n2 == null || n3 == null) {
+//     print('Iltimos, to‘g‘ri butun son kiriting!');
+//   } else {
+//     int engKichik = n1;
+//     if (n2 < engKichik) engKichik = n2;
+//     if (n3 < engKichik) engKichik = n3;
+
+//     print('Eng kichigi: $engKichik');
+//   }
+// }
